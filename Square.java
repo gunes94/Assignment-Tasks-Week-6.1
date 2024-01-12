150421ecadbb2b7516d02f2680fb36ea632dadbf
Gyunesh Mehmed <30699247+gunes94@users.noreply.github.com>
2024-01-12 18:42:21 +0200

    Create Square.java

---
+++ Square.java
@@ -0,0 +1,20 @@
+public class Square extends Rectangle{
+
+  private int width;
+  private int height;
+  public Square(int width, int height) {
+    super(width, height);
+    this.width = width;
+    this.height = height;
+  }
+
+  @Override
+  public int getArea() {
+    return width * height;
+  }
+
+  @Override
+  public int getPerimeter() {
+    return width * 2 + height * 2;
+  }
+}
