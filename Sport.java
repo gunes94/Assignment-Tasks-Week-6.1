8a88d45f0e4c05c7984efc739af743f9eea9c125
Gyunesh Mehmed <30699247+gunes94@users.noreply.github.com>
2024-01-12 18:44:30 +0200

    Create Sport.java

---
+++ Sport.java
@@ -0,0 +1,9 @@
+public class Sport {
+    public String getName(){
+      return "General Sport";
+    }
+
+    public String getNumberOfTeamMembers(){
+      return "Each team has n players in " + getName();
+    }
+}
