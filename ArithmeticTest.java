3f899e205066146849d5b139139d4a0cad57df62
Gyunesh Mehmed <30699247+gunes94@users.noreply.github.com>
2024-01-12 18:39:53 +0200

    Create ArithmeticTest.java

---
+++ ArithmeticTest.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ArithmeticTest {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    Arithmetic arithmetic = new Adder(2, 5);
+
+    System.out.println(arithmetic.getClass());
+    System.out.println(arithmetic.getClass().getSuperclass());
+
+    arithmetic.add(5);
+  }
+}
