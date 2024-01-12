6cf03a0967676d0e96fb1c447494b851b373305a
Gyunesh Mehmed <30699247+gunes94@users.noreply.github.com>
2024-01-12 18:40:32 +0200

    Create PrimeChecker.java

---
+++ PrimeChecker.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int N = scanner.nextInt();
+    int number;
+
+    PrimeChecker primeChecker = new PrimeChecker();
+    List<Integer> numbers = new ArrayList<>();
+
+    for (int i = 0; i < N; i++) {
+      number = scanner.nextInt();
+      numbers.add(number);
+    }
+    primeChecker.checkPrime(numbers);
+
+  }
+
+}
